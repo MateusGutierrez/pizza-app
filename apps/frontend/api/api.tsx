d9bc4f49e989a96bffe2ad6e842fff87830bafa0
Mateus Gutierrez <mateus.gutierrez@lagoasoft.com.br>
2024-08-24 18:59:18 -0300

    feat: add .env file to frontend

--- apps/frontend/api/api.tsx
+++ apps/frontend/api/api.tsx
@@ -1,7 +1,12 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const baseURL = process.env.BASE_URL
 
 export const Api = axios.create({
-    baseURL: "http://localhost:4000/",
+    baseURL: baseURL,
     timeout: 8000,
 });
 
